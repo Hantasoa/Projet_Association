533023f27c4f1b9223701c4919b3526cce7d5e65
Hantasoa <hantasoarafidimanana@gmail.com>
2018-04-14 14:08:58 +0300

    Hantasoa

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,5 +39,48 @@ $(document).ready(function(){
 		$('#div3').fadeIn(2000);
 	});
 
+	$('#ex').click(function(){
+		
+		$('.bloc').hide(1000);
+		//$('#div2').removeAttr('style');
+		$('#div4').show(1000);
+		
+	});
+	$('#fi').click(function(){
+		
+		$('.bloc').hide(1000);
+		//$('#div2').removeAttr('style');
+		$('#div5').show(1000);
+		
+	});
+	$('#evo').click(function(){
+		
+		$('.bloc').hide(1000);
+		//$('#div2').removeAttr('style');
+		$('#div6').show(1000);
+		
+	});
+	$('#gest').click(function(){
+		
+		$('.bloc').hide(1000);
+		//$('#div2').removeAttr('style');
+		$('#div7').show(1000);
+		
+	});
+	$('#ress').click(function(){
+		
+		$('.bloc').hide(1000);
+		//$('#div2').removeAttr('style');
+		$('#div8').show(1000);
+		
+	});
+	$('#diss').click(function(){
+		
+		$('.bloc').hide(1000);
+		//$('#div2').removeAttr('style');
+		$('#div9').show(1000);
+		
+	});
+
 	
 });
